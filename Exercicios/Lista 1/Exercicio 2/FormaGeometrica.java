5182ac44491d7a9f07c670884603537663bba0b9
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-07-29 11:37:56 -0300

    Create FormaGeometrica.java

---
+++ Exercicios/Lista 1/Exercicio 2/FormaGeometrica.java
@@ -0,0 +1,31 @@
+package Polimorfismo;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class FormaGeometrica {
+    private float lado;
+    protected float area;
+
+    public float getLado() {
+        return lado;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+    
+    public float calcularArea(){
+        area = lado * lado;
+        return area;
+    }
+}
