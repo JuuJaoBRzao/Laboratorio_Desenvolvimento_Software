7436d0d5c0872042db2a8c7531213a5f18e3626e
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-07-29 11:38:42 -0300

    Create Principal.java

---
+++ Exercicios/Lista 1/Exercicio 2/Principal.java
@@ -0,0 +1,27 @@
+package Polimorfismo;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Principal {
+    public static void main(String[] args) {
+        FormaGeometrica quadrado = new FormaGeometrica();
+        quadrado.setLado(3.44f);
+        
+        float area;
+        area = quadrado.calcularArea();
+        System.out.println("Area do quadrado: " +area);
+        
+        Triangulo t = new Triangulo();
+        t.setAltura(4.5f);
+        t.setBase(3.5f);
+        
+        area = t.calcularArea();
+        
+        System.out.println("Area do triangulo: " +area);
+            
+        
+    }
+    
+}
